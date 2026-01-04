98fd86adf46eb143bdb0b306dcd9dcca2f601d79
Poloqwq <fuxang.yai@gmail.com>
2026-01-04 13:39:55 +0800

    student version

--- backend/routers/app.py
+++ backend/routers/app.py
@@ -139,7 +139,8 @@ def deposit():
     deposit_amount = request.form['amount']
     if(username and bank.account_db.get_account_info(username)):
         # Implement deposit logic here
-        bank.deposit(username, int(deposit_amount))
+        
+        
         return jsonify({'success': True})
     else:
         return jsonify({'error': 'Invalid token'}), 401
@@ -158,7 +159,7 @@ def withdraw():
         
         # Implement withdraw logic here
 
-        bank.withdraw(username, int(withdraw_amount))
+        
         return jsonify({'success': True})
     else:
         return jsonify({'error': 'Invalid token'}), 401
@@ -180,8 +181,7 @@ def transfer():
     
     if(username and bank.account_db.get_account_info(username)):
         # Implement transfer logic here
-        bank.transfer(username, target_account, int(transfer_amount))
-        bank.auth.create_transfer_history(username, target_account, transfer_amount)
+        
         return jsonify({'success': True})
     else:
         return jsonify({'error': 'Invalid token'}), 401
@@ -192,7 +192,7 @@ def get_transaction_history():
     token = request.headers.get('token')
     username = bank.auth.decode_jwt_token(token)
     if username and bank.account_db.get_account_info(username):
-        history = bank.account_db.get_account_transfer_history(username, 5)
+        history = "please implement transaction history retrieval qwq"
         return jsonify({'transaction_history': history})
     else:
         return jsonify({'error': 'Invalid token'}), 401
